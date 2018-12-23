0315f123247e65ce269045da7bde4c0844661863
Farhang Darzi <farhang.darzi@gmail.com>
2018-12-23 20:24:22 +0330

    Setting up karma and protractor enviroment and change one e2e test in order to work successfuly.

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -7,8 +7,8 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display home works!', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to ng-weather-app!');
+    expect(page.getParagraphText()).toEqual('header works!');
   });
 });
