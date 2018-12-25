ae9053a42aec4e444a9c343fa322b07965ff0fc0
Farhang Darzi <farhang.darzi@gmail.com>
2018-12-25 20:03:05 +0330

    Install ngx-toaster.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     HttpClientModule,
+    ToastrModule,
   ],
   exports: [
     CommonModule,
     HttpClientModule,
+    ToastrModule,
   ]
 })
 export class SharedModule {}
